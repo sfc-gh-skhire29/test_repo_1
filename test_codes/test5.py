c4a41669f0e60fa1c4a09d97890c37e894dda5fe
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-02-10 22:28:07 +0530

    adds test5.py

---
+++ test_codes/test5.py
@@ -0,0 +1,14 @@
+import sys,os,random,numpy
+class fruit:
+    def __init__(self,a):
+        print("s")
+
+     def flavour(self,f):
+        f="sweet"
+        for i in range(10):
+            print(f)    
+print("fruit")
+
+apple=fruit("apple")
+apple.flavour("sw")
+
