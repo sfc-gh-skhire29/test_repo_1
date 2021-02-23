acabb624a4a9cb4fd475dc1efad01bd78e38b8c5
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-02-23 12:47:16 +0530

    modifies gitdiff

--- gitdiff.sh
+++ gitdiff.sh
@@ -5,7 +5,5 @@ head=main
 test_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 echo "head branch: " $head
 echo "test/current branch: " $test_branch
-MODIFIED_FILES=$(git diff --name-only $test_branch $head | grep -r --include=*.{py,sql})
+MODIFIED_FILES=$(git diff --name-only $test_branch $head | grep -E "(${format})")
 echo $MODIFIED_FILES
-
-#-E "(${format})"
