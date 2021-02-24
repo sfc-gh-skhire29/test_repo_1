325d20418f57456490689eae7a3c4fd6e06e8995
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-02-24 12:20:30 +0530

    Update gitdiff.sh

--- gitdiff.sh
+++ gitdiff.sh
@@ -2,11 +2,11 @@
 
 format_py=.py
 format_sql=.sql
-head=main
-test_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-echo "head branch: "$head
-echo "test/current branch: "$test_branch
-MODIFIED_FILES_python=$(git diff --name-only $test_branch $head | grep -E "(${format_py})")
-MODIFIED_FILES_sql=$(git diff --name-only $test_branch $head | grep -E "(${format_sql})")
+target=main
+head=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo "target branch: "$target
+echo "test/current branch: "$head
+MODIFIED_FILES_python=$(git diff --name-only $head $target | grep -E "(${format_py})")
+MODIFIED_FILES_sql=$(git diff --name-only $head $target | grep -E "(${format_sql})")
 echo "Modified python files: "$MODIFIED_FILES_python
 echo "Modified sql files: "$MODIFIED_FILES_sql
