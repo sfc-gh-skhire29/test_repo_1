ced8af274e61d1e4189623e1812437e55ade5155
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-08-19 22:20:05 +0530

    code update

--- build/json_val.py
+++ build/json_val.py
@@ -1,15 +1,47 @@
+import argparse
 import json
 import os
+from jsonschema.exceptions import ValidationError
+from json.decoder import JSONDecodeError
 from jsonschema import validate
+
+my_parser = argparse.ArgumentParser(description='List the json files of a folder')
+my_parser.add_argument('Path',
+                       metavar='path',
+                       type=str,
+                       nargs = '+',
+                       help='the path')
+
+args = my_parser.parse_args()
+input_paths = args.Path
+print("Files Passed are ",input_paths)
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
 repo_path  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-s = open(os.path.join(dirpath,'complete_schema_crowdstrike.json'),'r')
-schema = json.load(s)
+schema_file_path = os.path.join(dirpath,'complete_schema_crowdstrike.json')
+try:
+    s = open(schema_file_path,'r')
+    schema = json.load(s)
+except JSONDecodeError as jsonerror:
+    print("---------------------------------------------------------------------------")
+    print("JSONDecodeError in SCHEMA FILE ", schema_file_path)
+    print(jsonerror)
 
-f = open(os.path.join(repo_path, 'json_files/example.json'),'r')
-example = json.load(f)
+for input_file in input_paths:
+    try: 
+        f = open(os.path.join(repo_path, input_file),'r')
+        example = json.load(f)
+    except JSONDecodeError as jsonerror:
+        print("---------------------------------------------------------------------------")
+        print("JSONDecodeError in ", input_file)
+        print(jsonerror)
+            
+    try:
+        validate(instance = example, schema = schema)
+    
 
-
-validate(instance = example, schema = schema)
-
+    except ValidationError as valerrr:
+        print("---------------------------------------------------------------------------")
+        print("Schema ValidationError in ", input_file)
+        print(valerrr)
+    f.close()
