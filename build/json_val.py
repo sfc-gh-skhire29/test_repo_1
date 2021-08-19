a6ba719658d07aec7075ef13904ddeeb02680453
Sarvesh Khire <sarvesh.khire@snowflake.com>
2021-08-19 21:34:21 +0530

    adds json schemas

--- build/json_val.py
+++ build/json_val.py
@@ -1,11 +1,13 @@
 import json
+import os
 from jsonschema import validate
 
-
-s = open('schema.json','r')
+dirpath = os.path.dirname(os.path.abspath(__file__))
+repo_path  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+s = open(os.path.join(dirpath,'complete_schema.json'),'r')
 schema = json.load(s)
-print(schema)
-f = open('./json_files/example.json','r')
+
+f = open(os.path.join(repo_path, 'json_files/example.json'),'r')
 example = json.load(f)
 
 
